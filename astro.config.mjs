ba608192885eb86ade8993a564080e5373e506f4
Agus Altamirano <agusalta002@gmail.com>
2025-02-26 17:20:47 -0300

    Add vercell, fix fonts url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   experimental: {
     svg: true,
   },
+
   prefetch: true,
   integrations: [react()],
+  adapter: vercel(),
 });
