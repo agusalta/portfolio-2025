316698472e21ebbb082a0e7e3456bcbd618842bc
Agus Altamirano <agusalta002@gmail.com>
2025-02-26 17:40:24 -0300

    Fix fonts layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
